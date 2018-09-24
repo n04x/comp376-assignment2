4a6b95f5d7f352213a47ebfc7bb6a35547ad9762
noax <thomasbacks@gmail.com>
2018-09-24 19:47:12 -0400

    Decide to go with Star Wars theme game, not finalized yet waiting on teacher feedback

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class SpaceShip : MonoBehaviour {
 	
 	public float speed;
-	private float limitX = 6;
-	private float limitY = 4;
+	private float HORIZONTAL_LIMIT = 6;
+	private float VERTICAL_LIMIT = 4;
 	Rigidbody2D ssRigidbody;
 	// Use this for initialization
 	void Start () {
@@ -15,16 +15,17 @@ public class SpaceShip : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// move our space ship around
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
 		Vector2 movement = new Vector2(horizontal, vertical);
-		
 		ssRigidbody.velocity = movement * speed;
 
+		// check if the screen boundary is respected
 		ssRigidbody.position = new Vector3(
-			Mathf.Clamp(ssRigidbody.position.x, -limitX, limitX), 
-			Mathf.Clamp(ssRigidbody.position.y, -limitY, limitY),
-			0.0f
+			Mathf.Clamp(ssRigidbody.position.x, -HORIZONTAL_LIMIT, HORIZONTAL_LIMIT), 
+			Mathf.Clamp(ssRigidbody.position.y, -VERTICAL_LIMIT, VERTICAL_LIMIT),
+			0.0f	// Since the z axis will never change we set it to 0.
 		);
 	}
 }
