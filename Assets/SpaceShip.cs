b14e83deea3bdf3c4716ecdee030ba543d72cc70
noax <thomasbacks@gmail.com>
2018-09-24 16:19:46 -0400

    Fixes #2

---
+++ Assets/SpaceShip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+	
+	public float speed;
+	private float minX = -5;
+	private float maxX = 5;
+	private float minY = -5;
+	private float maxY = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector2 movement = new Vector2(horizontal, vertical);
+		
+		GetComponent<Rigidbody2D>().velocity = movement * speed;
+	}
+}
