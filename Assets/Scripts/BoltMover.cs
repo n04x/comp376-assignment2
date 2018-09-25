91ea9b35346e9958f781b0f84a3b6ad092b8022b
noax <thomasbacks@gmail.com>
2018-09-24 22:00:18 -0400

    Add laser bolt prefab, bolt logic and create its material as well

---
+++ Assets/Scripts/BoltMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltMover : MonoBehaviour {
+	Rigidbody2D bRigidbody2D;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		bRigidbody2D = GetComponent<Rigidbody2D>();
+		bRigidbody2D.velocity = transform.up * speed;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
