53fb1ca167e391477b8bcf7629193e15d6d9aa61
noax <thomasbacks@gmail.com>
2018-09-26 16:32:18 -0400

    Add TIE Fighter which will be our enemy A based on PDF, add explosion animation for when we destroy a TIE Fighter. Fixes #5

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+	
+	public GameObject explosion;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Boundary") {
+			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+		Destroy(explosion);
+	}
+}
---
+++ Assets/Scripts/DestroyExplosion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyExplosion : MonoBehaviour {
+
+	private IEnumerator KillOnAnimationEnd() {
+		yield return new WaitForSeconds(1.2f);
+		Destroy(gameObject);
+	}
+
+	void Update()
+	{
+		StartCoroutine(KillOnAnimationEnd());		
+	}
+}
