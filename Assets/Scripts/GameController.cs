d3a7d2784114c7308ea5bf3d14268f7405642ac4
noax <thomasbacks@gmail.com>
2018-09-26 19:00:49 -0400

    Add a Game Controller in the project to handle all ours stuff. So far we have added our spawning wave of enemy A which are TIE figthers.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	
+	public GameObject enemyA;
+	public Vector3 spawnPosition;
+	public int wavesA;
+	
+	void Start() {
+		SpawnEnemyA();
+	}
+
+	void SpawnEnemyA() {
+
+		for(;;) {
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate(enemyA, spawnPosition, spawnRotation);
+		} 
+	}
+}
