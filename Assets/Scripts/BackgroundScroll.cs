e258af3608e0022058a5872e181bf97f647fa56f
noax <thomasbacks@gmail.com>
2018-09-26 20:10:29 -0400

    Add a scrolling background for the game. #18, will need to add a stop condition

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour {
+	public Renderer rend;
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * speed;
+
+		rend.material.mainTextureOffset = new Vector2(0, offset);	
+	}
+}
