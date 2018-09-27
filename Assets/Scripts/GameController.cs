aac9f42539e4d590d05043e715204feeee231c0e
noax <thomasbacks@gmail.com>
2018-09-27 11:15:24 -0400

    Fixes #19 all issue with Star Destroyer is resolved, need to implement shooting from the star destroyer

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 	
-	public GameObject enemyA;
-	public Vector3 spawnPosition;
+	public GameObject tie_fighter_object;
+	public GameObject star_destroyer_object;
+	public Vector3 tie_fighter_spawn_pos;
+	public Vector3 star_destroyer_spawn_pos;
 	float startWait = 1.0f;
-	float spawnWait = 5.0f;
+	float tie_fighter_spawnWait = 5.0f;
+	float star_destroyer_startWait = 20.0f;
+	float star_destroyer_spawnWait = 2.0f;
 	
 	void Start() {
-		StartCoroutine(SpawnEnemyA());
+		StartCoroutine(SpawnTIEFighter());
 		// SpawnEnemyA();
 	}
 
-	IEnumerator SpawnEnemyA() {
+	IEnumerator SpawnTIEFighter() {
 		yield return new WaitForSeconds(startWait);
 		for(int i = 0; i < 3; i++) {
 			Quaternion spawnRotation = Quaternion.identity;
-			Instantiate(enemyA, spawnPosition, spawnRotation);
-			yield return new WaitForSeconds(spawnWait);
+			Instantiate(tie_fighter_object, tie_fighter_spawn_pos, spawnRotation);
+			yield return new WaitForSeconds(tie_fighter_spawnWait);
+		}
+	}
+
+	IEnumerator SpawnStarDestroyer() {
+		yield return new WaitForSeconds(star_destroyer_startWait);
+		for(int i = 0; i < 5; i++) {
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate(star_destroyer_object, star_destroyer_spawn_pos, spawnRotation);
+			yield return new WaitForSeconds(star_destroyer_spawnWait);
 		}
 	}
 }
--- Assets/Scripts/StarDestroyerMovement.cs
+++ Assets/Scripts/StarDestroyerMovement.cs
@@ -6,9 +6,12 @@ public class StarDestroyerMovement : MonoBehaviour {
 	public float speedY;
 	public float distance;
 	public float speedX;
+	Rigidbody2D ssrgb2d;
 	
 	private Vector3 _startPos; 
 	void Start() {
+		ssrgb2d = GetComponent<Rigidbody2D>();
+		ssrgb2d.velocity = transform.right * speedX;
 		_startPos = transform.position;
 	}
 	// Update is called once per frame
