112470a781ca2b39af405bf8f593746fbe26443c
noax <thomasbacks@gmail.com>
2018-10-01 11:43:28 -0400

    Added Scoreboard and all it's requirement, now enemyA gives 100 points, enemyB gives 200 points. Fixes #26

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class DestroyByContact : MonoBehaviour {
 	
 	public GameObject explosion;
+	public int score_value;
+	private GameController game_controller;
+	
+	void Start() {
+		GameObject game_controller_object = GameObject.FindWithTag("GameController");
+		if(game_controller_object != null) {
+			game_controller = game_controller_object.GetComponent<GameController>();
+		}	
+		if(game_controller == null) {
+			Debug.Log("cant find 'GameController' script");
+		}
+	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.tag == "Boundary") {
 			return;
 		}
 		Instantiate(explosion, transform.position, Quaternion.identity);
+		game_controller.AddScore(score_value);
 		Destroy(other.gameObject);
 		Destroy(gameObject);
 		Destroy(explosion);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,23 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 	
 	public GameObject tie_fighter_object;
 	public GameObject star_destroyer_object;
 	public GameObject death_star_object;
+
+	public Text score_board;
+	
 	public Vector3 tie_fighter_spawn_pos;
 	public Vector3 star_destroyer_spawn_pos;
-
 	public Vector3 death_star_spawn_pos;
+
 	float startWait = 1.0f;
 	float tie_fighter_spawnWait = 8.0f;
 	float star_destroyer_startWait = 20.0f;
-	float death_star_startWait = 40.0f   ;
-	// float star_destroyer_spawnWait = 4.0f;
+	float death_star_startWait = 40.0f;
+
+	private int score;
 	
 	void Start() {
+		score = 0;
+		UpdateScore();
 		StartCoroutine(SpawnTIEFighter());
 		StartCoroutine(SpawnStarDestroyer());
 		StartCoroutine(SpawnDeathStar());
@@ -43,5 +50,15 @@ public class GameController : MonoBehaviour {
 		yield return new WaitForSeconds(death_star_startWait);
 		Quaternion spawnRotation = Quaternion.identity;
 		Instantiate(death_star_object, death_star_spawn_pos, spawnRotation);
+	}
+
+	// this will be used for other elements in order to update our scoreboard.
+	public void AddScore(int new_score_val) {
+		score += new_score_val;
+		UpdateScore();
+	}
+
+	void UpdateScore() {
+		score_board.text = "Score: " + score;
 	}
 }
