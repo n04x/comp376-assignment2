3215b6e679f6d2956097ad9b9e8d3ab9aa6aabe7
noax <thomasbacks@gmail.com>
2018-10-02 21:51:45 -0400

    Add R2D2 script to handle collision, will need to add powerup too.

---
+++ Assets/Scripts/R2D2PowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class R2D2PowerUp : MonoBehaviour {
+Rigidbody2D r2d2rgbd2D;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		r2d2rgbd2D = GetComponent<Rigidbody2D>();
+		r2d2rgbd2D.velocity = transform.up * speed;		
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if(other.gameObject.tag == "Boundary") {
+			return;
+		}
+		Debug.Log(other.tag);
+		Destroy(gameObject);
+	}
+}
